131d88afde7f4c58614d12a3a2d7e5f082457b70
Pasieka Manuel <mapa17@posgrado.upv.es>
2013-02-15 15:58:46 +0100

    Adding new gui

--- src/gflyMAD.py
+++ src/gflyMAD.py
@@ -19,7 +19,7 @@ class UI:
     def __init__(self):
         me = os.path.dirname(os.path.abspath(__file__))
         self._ui = Gtk.Builder()
-        self._ui.add_from_file(os.path.join(me,"gflyMAD.glade"))
+        self._ui.add_from_file(os.path.join(me,"gflyMAD2.glade"))
 
         #Workaround, keep references of all rosgobject elements
         self._refs = []
@@ -32,66 +32,67 @@ class UI:
 
     def _build_ui(self):
         
-        nodeName = rospy.get_param('~nodeName') + "/"
-        deviceName = "/" + rospy.get_param('~deviceName') + "/"
-        
-        rospy.loginfo("Starting gstroke with node name %s and device name %s" % (nodeName, deviceName) )
-        
-        gdeviceName = self._ui.get_object("deviceName")
-        gdeviceName.set_text(rospy.get_param('~deviceName'))
+        nodepath = "flyMAD"
+        package = "flyMAD"
+        calibrationFile = "calibrationOUT"
         
         self._refs.append( GtkButtonStartNode(
-                widget=self._ui.get_object("start"),
-                nodepath="strokelitude2",
+                widget=self._ui.get_object("bFlyTrax"),
+                nodepath=nodepath,
                 nodemanager=self._manager,
-                package="strokelitude2",
-                node_type="imagenode" )
-                #,args='_movie:="/media/DATA/Strokelitude2/movies/movie20121219_164901.fmf"' )
+                package=package,
+                node_type="fview"
+                ,args='_movie:="/media/DATA/Strokelitude2/movies/movie20121219_164901.fmf"' )
                 )
         
-        self._refs.append(
-                GtkEntryTopicWidget(
-                widget=self._ui.get_object("FPS"),
-                nodepath = nodeName + "framerate",
-                msgclass=std_msgs.msg.Float32,
-                format_func=lambda x:"{0:5.1f} fps".format(x.data) )
+        self._refs.append( GtkButtonStartNode(
+                widget=self._ui.get_object("bMicro"),
+                nodepath=nodepath,
+                nodemanager=self._manager,
+                package=package,
+                node_type="flymad_micro" )
                 )
         
-        self._refs.append(
-        GtkButtonKillNode(
-                widget=self._ui.get_object("stop"),
-                nodepath= ''.join(nodeName.split())[:-1],
+        self._refs.append( GtkButtonStartNode(
+                widget=self._ui.get_object("bStartCalibration"),
+                nodepath=nodepath,
+                nodemanager=self._manager,
+                package=package,
+                node_type="generate_calibration",
+                args=calibrationFile )
+                )
+                
+        self._refs.append( GtkButtonKillNode(
+                widget=self._ui.get_object("bStopCalibration"),
+                nodepath= nodepath,
                 nodemanager=self._manager )
-                          )
+                )
         
-        self._refs.append(
-        GtkEntryParam(
-                widget=self._ui.get_object("trigger"),
-                nodepath=deviceName + "trigger" )
-                          )
-        
-        self._refs.append(
-        GtkSpinButtonParam(
-                widget=self._ui.get_object("gain"),
-                nodepath=deviceName + "gain" )
-                          )
-        
-        self._refs.append(
-        GtkSpinButtonParam(
-                widget=self._ui.get_object("shutter"),
-                nodepath=deviceName + "shutter" )
-                          )
-        
-        self._refs.append(
-        GtkButtonStartNode(
-                widget=self._ui.get_object("glView"),
-                nodepath="gl_view",
+        self._refs.append( GtkButtonStartNode(
+                widget=self._ui.get_object("bCleanCalibration"),
+                nodepath=nodepath,
                 nodemanager=self._manager,
-                package="strokelitude2",
-                node_type="gl_view",
-                args="image_raw:={}/image_raw".format(nodeName) )
-                          )
-               
+                package=package,
+                node_type="filter_calibration" )
+                )
+        
+        self._refs.append( GtkButtonStartNode(
+                widget=self._ui.get_object("bTracker"),
+                nodepath=nodepath,
+                nodemanager=self._manager,
+                package=package,
+                node_type="tracker" )
+                )
+                          
+        self._refs.append( GtkButtonStartNode(
+                widget=self._ui.get_object("bTargeter"),
+                nodepath=nodepath,
+                nodemanager=self._manager,
+                package=package,
+                node_type="targeter" )
+                )
+        
+        
 
 if __name__ == "__main__":
     rospy.init_node("gflyMAD", anonymous=True)
