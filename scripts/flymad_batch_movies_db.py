2e17f0c52b1d4bafc68d8086d438cbd8180d9345
Andrew Straw <strawman@astraw.com>
2013-07-15 13:39:03 +0200

    misc file found on computer (made by John?)

---
+++ scripts/flymad_batch_movies_db.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import time
+import os.path
+import os.environ
+import glob
+import tempfile
+import shutil
+
+import flymad_compositor
+import flymad_moviemaker
+
+def make_movie(wide,zoom,bag,imagepath,filename):
+    flymad_compositor.doit(wide,zoom,bag,imagepath=imagepath)
+    return flymad_moviemaker.doit(imagepath,finalmov=filename)
+
+if __name__ == "__main__":
+    BAG_DATE_FMT = "rosbagOut_%Y-%m-%d-%H-%M-%S.bag"
+    FMF_DATE_FMT = "%Y%m%d_%H%M%S.fmf"
+
+    destdir = os.path.expanduser("~/movies")
+    inputfmf = os.path("/media/DBATH 1TB/please_make_mp4s")
+    inputbags = os.path.expanduser("~/flymad_rosbag/")
+
+    fmfs = glob.glob(inputfmf+"/*.fmf")
+    bags = glob.glob(inputbags+"/*.bag")
+
+    for bag in bags:
+        btime = time.strptime(os.path.basename(bag), BAG_DATE_FMT)
+        matching = {}
+        for fmf in fmfs:
+            try:
+                fmffn = os.path.basename(fmf)
+                genotype,camn,datestr = fmffn.split("_",2)
+            except ValueError:
+                #print "invalid fmfname", fmf
+                continue
+
+            try:
+                fmftime = time.strptime(datestr, FMF_DATE_FMT)
+            except ValueError:
+                #print "invalid fmffname", fmf
+                continue
+
+            dt = abs(time.mktime(fmftime) - time.mktime(btime))
+
+            if (dt < 20):
+                matching[camn] = fmf
+                matching["genotype"] = genotype
+                matching["date"] = btime
+
+        if matching:
+            tmpdir = tempfile.mkdtemp()
+            destfn = os.path.join(destdir,
+                            "%s_%s.mp4" % (matching["genotype"], 
+                                           time.strftime("%Y%m%d_%H%M%S",matching["date"]))
+            )
+
+            print "making",destfn
+            try:
+                ok = make_movie(
+                        matching["wide"],
+                        matching["zoom"],
+                        bag,
+                        tmpdir,
+                        destfn
+                )
+            except:
+                pass
+
+            shutil.rmtree(tmpdir)
+
+
