6403e58230a067eed4582ba99d741c8dc52d38d5
John Stowers <john.stowers@gmail.com>
2013-10-22 21:17:07 +0200

    enforce jumb frames

--- nodes/launch_fview_wide.sh
+++ nodes/launch_fview_wide.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-fview --plugins=0,1,4
+
+if ifconfig eth1 | grep -q MTU:9000 ; then
+  echo "FVIEW RUNNING WITH JUMBO FRAMES - GOOD"
+  LIBCAMIFACE_ARAVIS_PACKET_SIZE=8000 fview --plugins=0,1,4
+else
+  echo "FVIEW RUNNING WITHOUT JUMBO FRAMES - BAD"
+  echo "you should type"
+  echo "sudo ifconfig eth1 mtu 9000 up"
+  fview
+fi
+
