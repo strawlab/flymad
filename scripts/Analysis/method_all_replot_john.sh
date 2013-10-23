efcd098ee92bb79c273da334d069f2d3046e2a45
John Stowers <john.stowers@gmail.com>
2013-10-23 15:38:26 +0200

    danplots: remove my plotting script, it can live in the integration branch

--- scripts/Analysis/method_all_replot_john.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python flymad_speed_multiplot.py ~/Dropbox/Straw/FLYMAD_data_prep_john/DATA_speed --only-plot --show &
-python flymad_velocity_multiplot.py ~/Dropbox/Straw/FLYMAD_data_prep_john/DATA_velocity/ --only-plot --show &
-python flymad_courtship_plots_10minexp.py ~/Dropbox/Straw/scored_data/courtship_10min/ --only-plot --show &
-
-wait
