d38b120e26f806c937ced4fed7640439d1260bb5
John Stowers <john.stowers@gmail.com>
2013-11-22 16:03:47 +0100

    test code for vel and smoothing

---
+++ scripts/analysis/test/testsmooth.py
@@ -0,0 +1,118 @@
+import madplot
+
+import sys
+import json
+import os.path
+import matplotlib.pyplot as plt
+
+import madplot
+
+BAG = '/mnt/strawscience/data/FlyMAD/MW/14_11/Moonw_b_5_90/2013-11-14-01-43-31.bag'
+oid = 18913
+
+arena = madplot.Arena('mm')
+
+def get_df(smooth):
+
+    smoothstr = {True:'smooth',False:'nosmooth'}[smooth]
+
+    l_df, t_df, h_df, geom = madplot.load_bagfile(BAG, arena, smooth=smooth)
+
+    fig = plt.figure('traj %s' % smoothstr)
+    fig.suptitle('trajectory %s' % smoothstr)
+    ax = fig.add_subplot(1,1,1)
+    madplot.plot_tracked_trajectory(ax, t_df, arena, debug_plot=False)
+    ax.add_patch(arena.get_patch(fill=False))
+    ax.set_xlabel('position (%s)' % arena.unit)
+    fig.savefig('testsmooth_mwb_pos_%s.png' % smoothstr)
+
+    df = t_df[t_df['tobj_id'] == oid]
+
+    #print df.iloc[39].index[0] - df.iloc[0]
+
+    times = [(ix - df.index[0]).total_seconds() for ix in df.index]
+
+    #print times[39]
+
+    ######yyyyyyyyyy
+    fig = plt.figure('y %s' % smoothstr,figsize=(10,8))
+    fig.suptitle('y %s' % smoothstr)
+    ax = fig.add_subplot(2,2,1)
+    ax.set_title('position')
+    ax.plot(times, df['y_px'].values)
+    ax.set_ylabel('y (px/s)')
+    ax = fig.add_subplot(2,2,2)
+    ax.set_title('converted position')
+    ax.plot(times, df['y'].values)
+    ax.set_ylabel('y (%s)' % arena.unit)
+    ax = fig.add_subplot(2,2,3)
+    ax.set_title('velocity')
+    ax.plot(times, df['vy_px'].values)
+    ax.set_ylabel('y-velocity (px/s)')
+    ax = fig.add_subplot(2,2,4)
+    ax.set_title('converted velocity')
+    ax.plot(times, df['vy'].values)
+    ax.set_ylabel('y-velocity (%s/s)' % arena.unit)
+    fig.savefig('testsmooth_mwb_y_%s.png' % smoothstr)
+
+    ######xxxxxxxxxxx
+    fig = plt.figure('x %s' % smoothstr,figsize=(10,8))
+    fig.suptitle('x %s' % smoothstr)
+    ax = fig.add_subplot(2,2,1)
+    ax.set_title('position')
+    ax.plot(times, df['x_px'].values)
+    ax.set_ylabel('x (px/s)')
+    ax = fig.add_subplot(2,2,2)
+    ax.set_title('converted position')
+    ax.plot(times, df['x'].values)
+    ax.set_ylabel('x (%s)' % arena.unit)
+    ax = fig.add_subplot(2,2,3)
+    ax.set_title('velocity')
+    ax.plot(times, df['vx_px'].values)
+    ax.set_ylabel('x-velocity (px/s)')
+    ax = fig.add_subplot(2,2,4)
+    ax.set_title('converted velocity')
+    ax.plot(times, df['vx'].values)
+    ax.set_ylabel('x-velocity (%s/s)' % arena.unit)
+    fig.savefig('testsmooth_mwb_x_%s.png' % smoothstr)
+
+    #######
+    df = t_df[t_df['tobj_id'] == oid].resample('500L')
+    times = [(ix - df.index[0]).total_seconds() for ix in df.index]
+
+    fig = plt.figure('speed %s' % smoothstr,figsize=(8,4))
+    fig.suptitle('speed %s' % smoothstr)
+    ax = fig.add_subplot(1,2,1)
+    ax.plot(times, df['v_px'].values)
+    ax.set_ylabel('speed (px/s)')
+    ax = fig.add_subplot(1,2,2)
+    ax.plot(times, df['v'].values)
+    ax.set_ylabel('speed (%s/s)' % arena.unit)
+    fig.savefig('testsmooth_mwb_speed_%s.png' % smoothstr)
+
+    return t_df[t_df['tobj_id'] == oid]
+
+if __name__ == "__main__":
+
+    smoothed = get_df(True)
+    unsmoothed = get_df(False)
+
+    fig = plt.figure('both')
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title("comparing smoothed and not")
+
+    ax.set_aspect('equal')
+    xlim,ylim = arena.get_limits()
+    ax.set_xlim(*xlim)
+    ax.set_ylim(*ylim)
+
+    ds = 1
+    ax.plot(smoothed['x'].values[::ds], smoothed['y'].values[::ds], 'r.')
+    ax.plot(unsmoothed['x'].values[::ds], unsmoothed['y'].values[::ds], 'b.')
+    fig.savefig('testsmooth_mwb_compare.png')
+
+    plt.show()
+
+
+
+
---
+++ scripts/analysis/test/testvel.py
@@ -0,0 +1,106 @@
+import madplot
+
+import sys
+import json
+import os.path
+import matplotlib.pyplot as plt
+
+import madplot
+
+BAG = '/mnt/strawscience/data/FlyMAD/velocity_test/2013-11-21-18-00-00.bag'
+oid = 52251
+
+smooth = True
+smoothstr = {True:'smooth',False:'nosmooth'}[smooth]
+
+arena = madplot.Arena('mm')
+l_df, t_df, h_df, geom = madplot.load_bagfile(BAG, arena, smooth=smooth)
+
+fig = plt.figure('traj %s' % smoothstr)
+fig.suptitle('trajectory %s' % smoothstr)
+ax = fig.add_subplot(1,1,1)
+madplot.plot_tracked_trajectory(ax, t_df, arena, debug_plot=False)
+ax.add_patch(arena.get_patch(fill=False))
+ax.set_xlabel('position (%s)' % arena.unit)
+fig.savefig('dorothea_pos_%s.png' % smoothstr)
+
+df = t_df[t_df['tobj_id'] == oid].resample('100L')
+
+#print df.iloc[39].index[0] - df.iloc[0]
+
+times = [(ix - df.index[0]).total_seconds() for ix in df.index]
+
+#print times[39]
+
+######yyyyyyyyyy
+fig = plt.figure('y %s' % smoothstr,figsize=(10,8))
+fig.suptitle('y %s' % smoothstr)
+ax = fig.add_subplot(2,2,1)
+ax.set_title('position')
+ax.plot(times, df['y_px'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('y (px/s)')
+ax = fig.add_subplot(2,2,2)
+ax.set_title('converted position')
+ax.plot(times, df['y'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('y (%s)' % arena.unit)
+ax = fig.add_subplot(2,2,3)
+ax.set_title('velocity')
+ax.plot(times, df['vy_px'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('y-velocity (px/s)')
+ax = fig.add_subplot(2,2,4)
+ax.set_title('converted velocity')
+ax.plot(times, df['vy'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('y-velocity (%s/s)' % arena.unit)
+fig.savefig('dorothea_y_%s.png' % smoothstr)
+
+######xxxxxxxxxxx
+fig = plt.figure('x %s' % smoothstr,figsize=(10,8))
+fig.suptitle('x %s' % smoothstr)
+ax = fig.add_subplot(2,2,1)
+ax.set_title('position')
+ax.plot(times, df['x_px'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('x (px/s)')
+ax = fig.add_subplot(2,2,2)
+ax.set_title('converted position')
+ax.plot(times, df['x'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('x (%s)' % arena.unit)
+ax = fig.add_subplot(2,2,3)
+ax.set_title('velocity')
+ax.plot(times, df['vx_px'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('x-velocity (px/s)')
+ax = fig.add_subplot(2,2,4)
+ax.set_title('converted velocity')
+ax.plot(times, df['vx'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('x-velocity (%s/s)' % arena.unit)
+fig.savefig('dorothea_x_%s.png' % smoothstr)
+
+#######
+df = t_df[t_df['tobj_id'] == oid].resample('500L')
+times = [(ix - df.index[0]).total_seconds() for ix in df.index]
+
+fig = plt.figure('speed %s' % smoothstr,figsize=(8,4))
+fig.suptitle('speed %s' % smoothstr)
+ax = fig.add_subplot(1,2,1)
+ax.plot(times, df['v_px'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('speed (px/s)')
+ax = fig.add_subplot(1,2,2)
+ax.plot(times, df['v'].values)
+ax.axvline(3.9,color='k');ax.axvline(7.5,color='k');
+ax.set_ylabel('speed (%s/s)' % arena.unit)
+fig.savefig('dorothea_speed_%s.png' % smoothstr)
+
+
+plt.show()
+
+
+
+
