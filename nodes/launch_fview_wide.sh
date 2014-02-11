b2512f86802b3de744a2d3a6ffb78a1e079b8b07
hdorothea <dorothea.hormann@gmail.com>
2014-02-11 16:36:45 +0100

    flymad2: update launch files to new fview etc

--- nodes/launch_fview_wide.sh
+++ nodes/launch_fview_wide.sh
@@ -2,11 +2,11 @@
 
 if ifconfig eth1 | grep -q MTU:9000 ; then
   echo "FVIEW RUNNING WITH JUMBO FRAMES - GOOD"
-  LIBCAMIFACE_ARAVIS_PACKET_SIZE=8000 fview --plugins=0,1,4
+  LIBCAMIFACE_ARAVIS_PACKET_SIZE=8000 python /usr/bin/fview __name:=fview_trackem --plugins=trackem
 else
   echo "FVIEW RUNNING WITHOUT JUMBO FRAMES - BAD"
   echo "you should type"
   echo "sudo ifconfig eth1 mtu 9000 up"
-  fview
+  python /usr/bin/fview __name:=fview_trackem --plugins=trackem
 fi
 
--- nodes/launch_fview_zoom.sh
+++ nodes/launch_fview_zoom.sh
@@ -2,11 +2,11 @@
 
 if ifconfig eth2 | grep -q MTU:9000 ; then
   echo "FVIEW RUNNING WITH JUMBO FRAMES - GOOD"
-  LIBCAMIFACE_ARAVIS_PACKET_SIZE=8000 fview --plugins=5
+  LIBCAMIFACE_ARAVIS_PACKET_SIZE=8000 python /usr/bin/fview __name:=fview_ttm --plugins=fview_head_track
 else
   echo "FVIEW RUNNING WITHOUT JUMBO FRAMES - BAD"
   echo "you should type"
   echo "sudo ifconfig eth2 mtu 9000 up"
-  fview
+  python /usr/bin/fview __name:=fview_ttm --plugins=fview_head_track
 fi
 
