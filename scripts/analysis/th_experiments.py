82a28d77a98497c531a801016e7269a989900fbe
Andrew Straw <strawman@astraw.com>
2014-02-23 22:28:52 +0100

    update constants

--- scripts/analysis/th_experiments.py
+++ scripts/analysis/th_experiments.py
@@ -1,2 +1,11 @@
-DOROTHEA_NAME_RE_BASE = r'(?P<condition>.*)_(?P<condition_flynum>\d)_(?P<trialnum>\d)_(?P<datetime>.*).fmf'
-DOROTHEA_BAGDIR = '/mnt/strawscience/data/FlyMAD/revision_dorothea/20_02/New'
+import os
+
+opj = os.path.join
+
+_DOROTHEA_BASEDIR = '/mnt/strawscience/data/FlyMAD/revision_dorothea/TH_Gal4_experiments'
+
+DOROTHEA_NAME_RE_BASE = r'(?P<condition>.*)_(?P<condition_flynum>\d+)_(?P<trialnum>\d)_(?P<datetime>.*).fmf'
+
+DOROTHEA_BAGDIR = opj(_DOROTHEA_BASEDIR,'bags')
+DOROTHEA_FMFDIR = opj(_DOROTHEA_BASEDIR,'fmfs')
+DOROTHEA_MP4DIR = opj(_DOROTHEA_BASEDIR,'mp4s')
