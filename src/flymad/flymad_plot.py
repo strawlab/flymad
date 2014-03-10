c08597ad79da9684a01c0ed7865c783a0a0a401a
Andrew Straw <strawman@astraw.com>
2014-03-10 11:29:36 +0100

    don't assume signals are periodic when downsampling

--- src/flymad/flymad_plot.py
+++ src/flymad/flymad_plot.py
@@ -54,7 +54,7 @@ def plot_timeseries_with_activation(ax, exp=dict(), ctrl=dict(), exp2=dict(), ta
         if downsample == 1:
             return a
         else:
-            return scipy.signal.resample(a, len(a[::downsample]))
+            return scipy.signal.decimate(a,downsample)
 
     #zorder = 1 = back
     #FIXME: make controls black, but the other black, not perfect black
