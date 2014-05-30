strawlab/flymad

commit f15d381b437f8b99aa57140f8dcbdca1c4ff0a19

more analysis work

scripts/analysis/generate_mw_ttm_movies.py
@@ -22,6 +22,9 @@ import rosbag
 
 import madplot
 
+from th_experiments import DOROTHEA_NAME_RE_BASE
+DOROTHEA_NAME_REGEXP = re.compile(r'^' + DOROTHEA_NAME_RE_BASE + '$')
+
 USE_MULTIPROCESSING = True
 
 Pair = collections.namedtuple('Pair', 'fmf bag maxt')
@@ -122,7 +125,7 @@ def doit_using_framenumber(user_data):
 
     actual_w, actual_h = benu.utils.negotiate_panel_size_same_height(panels, TARGET_OUT_W)
 
-    moviemaker = madplot.MovieMaker(obj_id=os.path.basename(zoomf), fps=14)
+    moviemaker = madplot.MovieMaker(obj_id=os.path.basename(zoomf), fps=60)
     target_moviefname = moviemaker.get_target_movie_name(mdir)
     if os.path.exists(target_moviefname):
         print 'target %r exists: skipping movie'%(target_moviefname,)
@@ -163,7 +166,12 @@ def get_matching_bag(fmftime, bagdir):
     bags = []
     for bag in glob.glob(os.path.join(bagdir,'*.bag')):
         btime = madplot.strptime_bagfile(bag)
-        dt = abs(time.mktime(fmftime) - time.mktime(btime))
+        try:
+            dt = abs(time.mktime(fmftime) - time.mktime(btime))
+        except:
+            print 'fmftime',fmftime
+            print 'btime',btime
+            raise
         bags.append( (bag, dt) )
 
     #sort based on dt (smallest dt first)    
@@ -174,8 +182,6 @@ def get_matching_bag(fmftime, bagdir):
         return bag
     else:
         return None
-
-DOROTHEA_NAME_REGEXP = re.compile(r'^(?P<condition>.*)_(?P<condition_flynum>\d)_(?P<trialnum>\d)(?P<datetime>.*).fmf$')
 
 def get_matching_fmf_and_bag(#gt,
                              base_dir, maxtime=0):
@@ -191,7 +197,8 @@ def get_matching_fmf_and_bag(#gt,
             continue
         parsed_data = matchobj.groupdict()
         print '%s -> %s'%(fmfname,parsed_data)
-        bagdir = os.path.join(base_dir,'TH_Gal4_bagfiles')
+        #bagdir = os.path.join(base_dir,'TH_Gal4_bagfiles')
+        bagdir = base_dir
         if os.path.isdir(bagdir):
             #we found a directory with matching bag files
             fmftime = time.strptime(parsed_data['datetime'], FMF_DATE_FMT)

scripts/flymad_score_movie.py
@@ -33,7 +33,8 @@ import flymad.conv as bagconv
 # Create a single vlc.Instance() to be shared by (possible) multiple players.
 instance = vlc.Instance("--no-snapshot-preview --snapshot-format png")
 
-DOROTHEA_NAME_REGEXP = re.compile(r'^(?P<condition>.*)_(?P<condition_flynum>\d)_(?P<trialnum>\d)(?P<datetime>.*).fmf.mp4$')
+from analysis.th_experiments import DOROTHEA_NAME_RE_BASE
+DOROTHEA_NAME_REGEXP = re.compile(r'^' + DOROTHEA_NAME_RE_BASE + '.mp4$')
 
 class OCRThread(threading.Thread):
 

scripts/analysis/madplot.py
@@ -1209,7 +1209,7 @@ class MovieMaker:
                 "%s/movie.y4m" % self.tmpdir,
         )
 
-        moviefname = self.get_target_movie_name()
+        moviefname = self.get_target_movie_name(moviedir)
         sh.mv("-u", "%s/movie.mp4" % self.tmpdir, moviefname)
         return moviefname
 

scripts/analysis/th_experiments.py
@@ -1,0 +1,2 @@
+DOROTHEA_NAME_RE_BASE = r'(?P<condition>.*)_(?P<condition_flynum>\d)_(?P<trialnum>\d)_(?P<datetime>.*).fmf'
+DOROTHEA_BAGDIR = '/mnt/strawscience/data/FlyMAD/revision_dorothea/20_02/New'

commit fcc5bf132701e9c73ac3a672d39331b8aea8f47d

analysis: cleanup / revert ads-analysis-hacks
this is a partial revert of
    d2958c18de2dfd33169559d84c3f864a1aa8001b
    6a691d01a81f685fde105f612a0d11c2e68c3bb6
    f15d381b437f8b99aa57140f8dcbdca1c4ff0a19

which broke the scorer and generate_mw_ttm_movies making it
only work for the naming convention of the th experiments.
Go back to the original implementation that should continue
to work with Dans conventions

scripts/analysis/generate_mw_ttm_movies.py
@@ -20,15 +20,12 @@ import benu.utils
 import roslib; roslib.load_manifest('flymad')
 import rosbag
 import flymad.madplot as madplot
-from flymad.th_experiments import DOROTHEA_NAME_RE_BASE
-
-DOROTHEA_NAME_REGEXP = re.compile(r'^' + DOROTHEA_NAME_RE_BASE + '$')
 
 USE_MULTIPROCESSING = True
 
 Pair = collections.namedtuple('Pair', 'fmf bag maxt')
 
-FMF_DATE_FMT = "%Y%m%d_%H%M%S"
+FMF_DATE_FMT = "%Y%m%d_%H%M%S.fmf"
 
 assert benu.__version__ >= "0.1.0"
 
@@ -124,7 +121,7 @@ def doit_using_framenumber(user_data):
 
     actual_w, actual_h = benu.utils.negotiate_panel_size_same_height(panels, TARGET_OUT_W)
 
-    moviemaker = madplot.MovieMaker(obj_id=os.path.basename(zoomf), fps=60)
+    moviemaker = madplot.MovieMaker(obj_id=os.path.basename(zoomf), fps=30)
     target_moviefname = moviemaker.get_target_movie_name(mdir)
     if os.path.exists(target_moviefname):
         print 'target %r exists: skipping movie'%(target_moviefname,)
@@ -163,6 +160,7 @@ def make_movie(wide,zoom,bag,imagepath,filename):
 
 def get_matching_bag(fmftime, bagdir):
     bags = []
+
     for bag in glob.glob(os.path.join(bagdir,'*.bag')):
         btime = madplot.strptime_bagfile(bag)
         try:
@@ -182,25 +180,28 @@ def get_matching_bag(fmftime, bagdir):
     else:
         return None
 
-def get_matching_fmf_and_bag(#gt,
-                             base_dir, maxtime=0):
+def get_bag_re(gt):
+    #the non-control MW experiment movies are names Moonw_movie
+    #return re.compile("%s_movie_([abh+]{1,3})_([0-9_]{1,3})(2013)(.*)" % gt)
+    return re.compile("%s_([abh+]{1,3})_([0-9_]+)(2013)(.*)" % gt)
 
+def get_matching_fmf_and_bag(gt, base_dir, maxtime=0):
+
+    bag_re = get_bag_re(gt)
     matching = []
 
-    for fmffile in glob.glob(os.path.join(base_dir,'*.fmf')):
+    for fmffile in glob.glob(os.path.join(base_dir,'%s_*.fmf' % gt)):
         fmfname = os.path.basename(fmffile)
-        matchobj = DOROTHEA_NAME_REGEXP.match(fmfname)
+        matchobj = bag_re.search(fmfname)
         if matchobj is None:
             print "error: incorrectly named fmf file?", fmffile
-            print 'fmfname',fmfname
             continue
-        parsed_data = matchobj.groupdict()
-        print '%s -> %s'%(fmfname,parsed_data)
-        #bagdir = os.path.join(base_dir,'TH_Gal4_bagfiles')
-        bagdir = base_dir
+
+        target,trial,year,date = matchobj.groups()
+        bagdir = os.path.join(base_dir,'%s_%s_%s' % (gt, target, trial))
         if os.path.isdir(bagdir):
             #we found a directory with matching bag files
-            fmftime = time.strptime(parsed_data['datetime'], FMF_DATE_FMT)
+            fmftime = time.strptime("%s%s" % (year,date), FMF_DATE_FMT)
             bagfile = get_matching_bag(fmftime, bagdir)
             if bagfile is None:
                 print "no bag for",fmffile
@@ -216,7 +217,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('path', nargs=1, help='path to data (a dir of fmfs and subdir of bags)')
-#    parser.add_argument('--genotype', required=True, help='genotype (the prefix of the fmfs; cs, Moonw, etc)')
+    parser.add_argument('--genotype', required=True, help='genotype (the prefix of the fmfs; cs, Moonw, etc)')
     parser.add_argument('--disable-multiprocessing', action='store_true', default=False)
     parser.add_argument('--dry-run', action='store_true', default=False)
     parser.add_argument('--show-theta', action='store_true', default=False)
@@ -234,8 +235,7 @@ if __name__ == "__main__":
         args.outdir = os.path.join(path,'mp4s')
 
     matching = [(m,args.outdir,args.show_theta,args.show_velocity)\
-                for m in get_matching_fmf_and_bag(#args.genotype,
-                                                  path, args.max_time)]
+                for m in get_matching_fmf_and_bag(args.genotype, path, args.max_time)]
     print len(matching),"matching"
     if len(matching)==0:
         sys.exit(0)

scripts/flymad_score_movie.py
@@ -33,9 +33,6 @@ import flymad.conv as bagconv
 # Create a single vlc.Instance() to be shared by (possible) multiple players.
 instance = vlc.Instance("--no-snapshot-preview --snapshot-format png")
 
-from analysis.th_experiments import DOROTHEA_NAME_RE_BASE
-DOROTHEA_NAME_REGEXP = re.compile(r'^' + DOROTHEA_NAME_RE_BASE + '.mp4$')
-
 class OCRThread(threading.Thread):
 
     #set to zero to let gocr auto-threshold the image
@@ -426,7 +423,7 @@ if __name__ == '__main__':
         mode = OCRThread.MODE_FRAMENUMBER
 
     BAG_DATE_FMT = "%Y-%m-%d-%H-%M-%S.bag"
-    MP4_DATE_FMT = "%Y%m%d_%H%M%S"
+    MP4_DATE_FMT = "%Y%m%d_%H%M%S.mp4"
 
     if not os.path.exists('/usr/bin/gocr'):
         raise RuntimeError('you need to install gocr')
@@ -493,21 +490,13 @@ if __name__ == '__main__':
             bname = None
         else:
             mp4fn = os.path.basename(mp4)
-            matchobj = DOROTHEA_NAME_REGEXP.match(mp4fn)
-            if matchobj is None:
-                continue
-            parsed_data = matchobj.groupdict()
-            if parsed_data['trialnum'] is not None:
-                if int(parsed_data['trialnum']) > args.max_trial:
-                    continue
-            #genotype,datestr = mp4fn.split("_",1)
-            mp4time = time.strptime(parsed_data['datetime'], MP4_DATE_FMT)
-
+            genotype,datestr = mp4fn.split("_",1)
+            mp4time = time.strptime(datestr, MP4_DATE_FMT)
             bname = None
             if inputbags:
                 best_diff = np.inf
                 for bag in inputbags:
-                    bagtime = time.strptime(os.path.basename(bag), BAG_DATE_FMT)
+                    bagtime = time.strptime(os.path.basename(bag).replace('rosbagOut',''), BAG_DATE_FMT)
                     this_diff = abs(time.mktime(bagtime)-time.mktime(mp4time))
                     if this_diff < best_diff:
                         bname = bag