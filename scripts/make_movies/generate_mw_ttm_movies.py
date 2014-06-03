6e44f0e88f1ad6ee477fdf72f7d0b47f085e9200
John Stowers <john.stowers@gmail.com>
2014-06-03 21:21:30 +0200

    analysis: move all move scripts to the same dir

--- scripts/make_movies/generate_mw_ttm_movies.py
+++ scripts/make_movies/generate_mw_ttm_movies.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sys
 import time
 import os.path
@@ -20,21 +21,17 @@ import benu.utils
 import roslib; roslib.load_manifest('flymad')
 import rosbag
 import flymad.madplot as madplot
-from flymad.th_experiments import DOROTHEA_NAME_RE_BASE, DOROTHEA_BAGDIR, DOROTHEA_MP4DIR
-
-DOROTHEA_NAME_REGEXP = re.compile(r'^' + DOROTHEA_NAME_RE_BASE + '$')
 
 USE_MULTIPROCESSING = True
 
 Pair = collections.namedtuple('Pair', 'fmf bag maxt')
 
-FMF_DATE_FMT = "%Y%m%d_%H%M%S"
+FMF_DATE_FMT = "%Y%m%d_%H%M%S.fmf"
 
 assert benu.__version__ >= "0.1.0"
 
 TARGET_OUT_W, TARGET_OUT_H = 1280, 1024
 MARGIN = 2
-TZNAME = 'CET'
 
 class Assembler:
     def __init__(self, w, h, panels, wfmf, zfmf, moviemaker):
@@ -61,23 +58,16 @@ class Assembler:
         return png
 
 def doit_using_framenumber(user_data):
-    match, mp4_dir, show_theta, show_velocity, pre_frames, post_frames = user_data
+    match, mdir, show_theta, show_velocity = user_data
 
     zoomf = match.fmf
     rosbagf = match.bag
     maxt = match.maxt
 
-    moviemaker = madplot.MovieMaker(basename=os.path.basename(zoomf), fps=15)
-    target_moviefname = moviemaker.get_target_movie_name(mp4_dir)
-    if os.path.exists(target_moviefname):
-        print 'target %r exists: skipping movie'%(target_moviefname,)
-        moviemaker.cleanup()
-        return
-
     arena = madplot.Arena(False)
     zoom = madplot.FMFImagePlotter(zoomf, 'z_frame')
     zoom.enable_color_correction(brightness=15, contrast=1.5)
-    wide = madplot.ArenaPlotter(arena,tzname=TZNAME)
+    wide = madplot.ArenaPlotter(arena)
 
     wide.show_theta = show_theta
     wide.show_velocity = show_velocity
@@ -89,44 +79,10 @@ def doit_using_framenumber(user_data):
     renderlist = []
 
     zoom_ts = zoom.fmf.get_all_timestamps().tolist()
-    df = madplot.load_bagfile_single_dataframe(rosbagf, arena, ffill=True,
-                                               tzname=TZNAME)
+    df = madplot.load_bagfile_single_dataframe(rosbagf, arena, ffill=True)
     t0 = df.index[0].asm8.astype(np.int64) / 1e9
 
-    # find start and stop frames based on laser transition ----
-    laser_power = df['laser_power'].values
-    framenumber = df['h_framenumber'].values
-
-    laser_power = np.array( laser_power, copy=True )
-    laser_power[ np.isnan(laser_power) ] = 0
-
-    dlaser = laser_power[1:]-laser_power[:-1]
-    laser_transitions = np.nonzero(dlaser)[0]
-    startframenumber = -np.inf
-    stopframenumber = np.inf
-    if pre_frames is None:
-        assert post_frames is None
-    if pre_frames is not None:
-        assert post_frames is not None
-
-        for i,idx in enumerate(laser_transitions):
-            if i==0:
-                startframenumber = framenumber[idx-pre_frames]
-            elif i==1:
-                stopframenumber = framenumber[idx+post_frames]
-            else:
-                print 'ERROR: unknown laser transition in %r'%zoomf
-                moviemaker.cleanup()
-                return
-
-    # got start and stop frames -----
-
     for idx,group in df.groupby('h_framenumber'):
-        # limit movies to only frames we want
-        this_framenumber = group['h_framenumber'].values[0]
-        if not (startframenumber <= this_framenumber and this_framenumber <= stopframenumber):
-            continue
-
         #did we save a frame ?
         try:
             frameoffset = zoom_ts.index(idx)
@@ -151,10 +107,6 @@ def doit_using_framenumber(user_data):
 
             renderlist.append(desc)
 
-    if len(renderlist)==0:
-        moviemaker.cleanup()
-        return
-
     wide.t0 = t0
 
     panels = {}
@@ -170,8 +122,14 @@ def doit_using_framenumber(user_data):
 
     actual_w, actual_h = benu.utils.negotiate_panel_size_same_height(panels, TARGET_OUT_W)
 
+    moviemaker = madplot.MovieMaker(basename=os.path.basename(zoomf), fps=30)
+    target_moviefname = moviemaker.get_target_movie_name(mdir)
+    if os.path.exists(target_moviefname):
+        print 'target %r exists: skipping movie'%(target_moviefname,)
+        return
+
     ass = Assembler(actual_w, actual_h,
-                    panels,
+                    panels, 
                     wide,zoom,
                     moviemaker,
     )
@@ -188,16 +146,18 @@ def doit_using_framenumber(user_data):
     if not USE_MULTIPROCESSING:
         pbar.finish()
 
-    if not os.path.exists(mp4_dir):
-        os.makedirs(mp4_dir)
+    if not os.path.exists(mdir):
+        os.makedirs(mdir)
 
-    moviefname = moviemaker.render(mp4_dir)
+    moviefname = moviemaker.render(mdir)
     print "wrote", moviefname
 
     moviemaker.cleanup()
+    
 
 def get_matching_bag(fmftime, bagdir):
     bags = []
+
     for bag in glob.glob(os.path.join(bagdir,'*.bag')):
         btime = madplot.strptime_bagfile(bag)
         try:
@@ -208,7 +168,7 @@ def get_matching_bag(fmftime, bagdir):
             raise
         bags.append( (bag, dt) )
 
-    #sort based on dt (smallest dt first)
+    #sort based on dt (smallest dt first)    
     bags.sort(key=operator.itemgetter(1))
     bag,dt = bags[0]
 
@@ -217,30 +177,35 @@ def get_matching_bag(fmftime, bagdir):
     else:
         return None
 
-def get_matching_fmf_and_bag(fmf_dir, maxtime,
-                             bagdir):
+def get_bag_re(gt):
+    #the non-control MW experiment movies are names Moonw_movie
+    #return re.compile("%s_movie_([abh+]{1,3})_([0-9_]{1,3})(2013)(.*)" % gt)
+    return re.compile("%s_([abh+]{1,3})_([0-9_]+)(2013)(.*)" % gt)
 
+def get_matching_fmf_and_bag(gt, base_dir, maxtime=0):
+
+    bag_re = get_bag_re(gt)
     matching = []
-    if not os.path.isdir(bagdir):
-        raise RuntimeError('bagdir not a directory')
 
-    for fmffile in glob.glob(os.path.join(fmf_dir,'*.fmf')):
+    for fmffile in glob.glob(os.path.join(base_dir,'%s_*.fmf' % gt)):
         fmfname = os.path.basename(fmffile)
-        matchobj = DOROTHEA_NAME_REGEXP.match(fmfname)
-
+        matchobj = bag_re.search(fmfname)
         if matchobj is None:
             print "error: incorrectly named fmf file?", fmffile
-            print 'fmfname',fmfname
             continue
-        parsed_data = matchobj.groupdict()
-        #print '%s -> %s'%(fmfname,parsed_data)
 
-        fmftime = time.strptime(parsed_data['datetime'], FMF_DATE_FMT)
-        bagfile = get_matching_bag(fmftime, bagdir)
-        if bagfile is None:
-            print "no bag for",fmffile
-        else:
+        target,trial,year,date = matchobj.groups()
+        bagdir = os.path.join(base_dir,'%s_%s_%s' % (gt, target, trial))
+        if os.path.isdir(bagdir):
+            #we found a directory with matching bag files
+            fmftime = time.strptime("%s%s" % (year,date), FMF_DATE_FMT)
+            bagfile = get_matching_bag(fmftime, bagdir)
+            if bagfile is None:
+                print "no bag for",fmffile
+            else:
                 matching.append( Pair(fmf=fmffile, bag=bagfile, maxt=maxtime) )
+        else:
+            print "no bags for",fmffile
 
     return matching
 
@@ -248,36 +213,26 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('fmfdir', nargs=1, help='path to data (a dir of fmfs and subdir of bags)')
-#    parser.add_argument('--genotype', required=True, help='genotype (the prefix of the fmfs; cs, Moonw, etc)')
+    parser.add_argument('path', nargs=1, help='path to data (a dir of fmfs and subdir of bags)')
+    parser.add_argument('--genotype', required=True, help='genotype (the prefix of the fmfs; cs, Moonw, etc)')
     parser.add_argument('--disable-multiprocessing', action='store_true', default=False)
     parser.add_argument('--dry-run', action='store_true', default=False)
     parser.add_argument('--show-theta', action='store_true', default=False)
     parser.add_argument('--show-velocity', action='store_true', default=False)
     parser.add_argument('--max-time', type=int, default=0, help='max time of video')
     parser.add_argument('--outdir', default=None, help='dir to save mp4s')
-    parser.add_argument('--bagdir', default=None, help='dir to save mp4s')
-    parser.add_argument('--n-frames-pre-laser', type=int, default=None)
-    parser.add_argument('--n-frames-post-laser', type=int, default=None)
 
     args = parser.parse_args()
-    fmfdir = args.fmfdir[0]
+    path = args.path[0]
 
-    if not os.path.isdir(fmfdir):
-        parser.error('fmfdir must be a directory')
-
-    if args.bagdir is None:
-        args.bagdir = DOROTHEA_BAGDIR
+    if not os.path.isdir(path):
+        parser.error('must be a directory')
 
     if args.outdir is None:
-        args.outdir = DOROTHEA_MP4DIR
+        args.outdir = os.path.join(path,'mp4s')
 
-    matching = [(m,args.outdir,args.show_theta,args.show_velocity,
-                 args.n_frames_pre_laser, args.n_frames_post_laser,
-                 )\
-                for m in get_matching_fmf_and_bag(fmfdir, args.max_time,
-                                                  args.bagdir,
-                                                  )]
+    matching = [(m,args.outdir,args.show_theta,args.show_velocity)\
+                for m in get_matching_fmf_and_bag(args.genotype, path, args.max_time)]
     print len(matching),"matching"
     if len(matching)==0:
         sys.exit(0)
@@ -296,3 +251,5 @@ if __name__ == "__main__":
     else:
         for match in matching:
             doit_using_framenumber(match)
+
+
