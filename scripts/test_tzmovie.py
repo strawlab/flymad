f51c1a45c003b6c084c7820e69cc8e6405385c3c
John Stowers <john.stowers@gmail.com>
2014-06-11 12:14:16 +0200

    add test

---
+++ scripts/test_tzmovie.py
@@ -0,0 +1,41 @@
+import subprocess
+import tempfile
+
+import roslib; roslib.load_manifest('flymad')
+
+import flymad.conv
+from flymad_score_movie import OCRThread
+
+class TestTZ:
+    def __init__(self, mp4, bag):
+        self.bag = bag
+        self.t = OCRThread(self._on_processing_finished, 'z', None, OCRThread.MODE_NORMAL, None)
+        subprocess.check_call("ffmpeg -i %s -vframes 1 -an -f image2 -y %s" % (mp4,self.t.input_image()),
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    shell=True)
+
+    def _on_processing_finished(self, err, now, ocrimg, key, tid):
+        self.now = now
+
+    def run(self):
+        self.t.start()
+        self.t.join()
+
+        df = flymad.conv.create_df(self.bag)
+
+        dfnow = df.index[0] / flymad.conv.SECOND_TO_NANOSEC
+
+        print self.now, dfnow
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mp4', metavar='movie', nargs=1)
+    parser.add_argument('bag', metavar='bag', nargs=1)
+    args = parser.parse_args()
+
+    t = TestTZ(args.mp4[0], args.bag[0])
+    t.run()
