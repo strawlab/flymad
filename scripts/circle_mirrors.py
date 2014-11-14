1b5714551ecc55f0761eac0e2a212a240c110c76
John Stowers <john.stowers@gmail.com>
2014-11-14 18:51:22 -0500

    circle_mirrors: adjustable range (and fix off-by-one)

--- scripts/circle_mirrors.py
+++ scripts/circle_mirrors.py
@@ -10,18 +10,19 @@ class Circler:
     def __init__(self):
         rospy.init_node('circler')
         self.pub = rospy.Publisher( 'flymad_micro/position', MicroPosition )
+        self.range = min(1.0, rospy.get_param('~range', default=0.5))
 
     def run(self):
         r = rospy.Rate(100) # 100hz
         tf = 0.1
         while not rospy.is_shutdown():
             t = rospy.get_time()
-            x = math.sin( tf*2*math.pi*t ) * 2**14
-            y = math.cos( tf*2*math.pi*t ) * 2**14
-            print x,y
+            x = math.sin( tf*2*math.pi*t ) * ((2**14)-1) * self.range
+            y = math.cos( tf*2*math.pi*t ) * ((2**14)-1) * self.range
             msg = MicroPosition()
             msg.posA = int(round(x))
             msg.posB = int(round(y))
+            msg.laser = 0xFF;
             self.pub.publish(msg)
             r.sleep()
 
