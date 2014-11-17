a6e8ee7985a8329f51ef4e137dcf013593f8faf6
John Stowers <john.stowers@gmail.com>
2014-11-17 18:29:33 -0500

    circle_lasers: implement /experiment/laser
    so that it can be launched from gflymad, and controlled
    completely using configure_lasers

--- scripts/circle_mirrors.py
+++ scripts/circle_mirrors.py
@@ -2,8 +2,10 @@
 import roslib; roslib.load_manifest('flymad')
 import rospy
 
+from std_msgs.msg import UInt8
 from flymad.msg import MicroPosition
-from flymad.constants import LASERS_ALL_ON
+from flymad.srv import LaserState, LaserStateResponse
+from flymad.constants import LASERS_ALL_ON, LASERS_ALL_OFF
 
 import math
 
@@ -12,6 +14,19 @@ class Circler:
         rospy.init_node('circler')
         self.pub = rospy.Publisher( 'flymad_micro/position', MicroPosition )
         self.range = min(1.0, rospy.get_param('~range', default=0.5))
+
+        self._laser = LASERS_ALL_OFF
+
+        _ = rospy.Subscriber('/experiment/laser',
+                            UInt8,
+                            self.on_laser)
+        _ = rospy.Service('/experiment/laser', LaserState, self.on_laser_srv)
+
+    def on_laser(self, msg):
+        self._laser = msg.data
+    def on_laser_srv(self, req):
+        self._laser = req.data
+        return LaserStateResponse()
 
     def run(self):
         r = rospy.Rate(100) # 100hz
@@ -23,7 +38,7 @@ class Circler:
             msg = MicroPosition()
             msg.posA = int(round(x))
             msg.posB = int(round(y))
-            msg.laser = LASERS_ALL_ON;
+            msg.laser = self._laser
             self.pub.publish(msg)
             r.sleep()
 
